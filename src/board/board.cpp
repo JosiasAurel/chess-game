937662615c582b87122ab77bfe80d0207d7874b7
JosiasAurel <ndjosiasaurel@gmail.com>
2023-04-06 19:06:16 +0100

    prototype board class

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -1,3 +1,10 @@
 #include "./board.hpp"
 
 array<array<Piece, 8>, 8> board;
+
+Board::Board() {}
+bool Board::move(Piece p, pair<int, int> target) { return false; }
+
+bool Board::ValidateMove(Piece p, pair<int, int> target) { return false; }
+
+GameState Board::EvaluateGame() { return GameState::CHECKMAKE; }
