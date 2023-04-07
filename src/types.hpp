7cb090812ae8b9cd730653b50b08b59b18fc168e
glenn-po <glennfor72@gmail.com>
2023-04-07 17:51:54 +0100

    Add type definitions and constants

---
+++ src/types.hpp
@@ -0,0 +1,15 @@
+#include <array>
+
+using std::array;
+
+//size of the chess
+const int GRID_SIZE = 8;
+
+//representing the internal state
+class Piece;
+using BoardState = array<array<Piece*, GRID_SIZE>, GRID_SIZE>;
+
+//represnting the team of a piece
+typedef enum {WHITE, BLACK} Team;
+
+
