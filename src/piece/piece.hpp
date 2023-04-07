4a1bec7c06c5a8a20f0f08eb51731394c9f9c495
JosiasAurel <ndjosiasaurel@gmail.com>
2023-04-07 18:00:52 +0100

    add s single include guard
    ensure headers get included just once

--- src/piece/piece.hpp
+++ src/piece/piece.hpp
@@ -1,64 +1,64 @@
+#pragma once
+
 #include "../types.hpp"
+#include <array>
 #include <string>
 #include <vector>
-#include <array>
 
-using std::string;
 using std::pair;
+using std::string;
 using std::vector;
-using std::array;
 
-class Piece{
-  private:
-    static unsigned counter;
-    unsigned id;
-    Team team;
-    //BoardState coordinates
-    pair<int, int> coordinate;
-    
-  public:
-  Piece(pair<int, int> position, Team side):
-    id (counter++), coordinate(position),team(side) {}
+class Piece {
+private:
+  static unsigned counter;
+  unsigned id;
+  Team team;
+  // BoardState coordinates
+  pair<int, int> coordinate;
+
+public:
+  Piece(pair<int, int> position, Team side)
+      : id(counter++), coordinate(position), team(side) {}
   ~Piece();
 
-  int getId(){return id;}
+  int getId() { return id; }
   bool move(BoardState, pair<int, int>);
-  virtual vector<pair<int, int>> getPossibleMoves(BoardState)=0;
-    
+  virtual vector<pair<int, int>> getPossibleMoves(BoardState) = 0;
 };
 
-class Pawn: private Piece{
-  public:
+class Pawn : private Piece {
+public:
   Pawn();
   ~Pawn(){};
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
-class Knight: private Piece{
-  public:
+class Knight : private Piece {
+public:
   Knight();
   ~Knight(){};
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
-class Bishop: private Piece{
-  public:
+class Bishop : private Piece {
+public:
   Bishop();
   ~Bishop(){};
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
-class Rook: private Piece{
-  public:
+class Rook : private Piece {
+public:
   Rook();
   ~Rook(){};
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
-class King: private Piece{
-  public:
+class King : private Piece {
+public:
   King();
   ~King(){};
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
-class Queen: private Piece{
-  public:
+class Queen : private Piece {
+public:
   Queen();
   ~Queen(){};
   vector<pair<int, int>> getPossibleMoves(BoardState);
--- src/types.hpp
+++ src/types.hpp
@@ -1,15 +1,15 @@
+#pragma once
+
 #include <array>
 
 using std::array;
 
-//size of the chess
+// size of the chess
 const int GRID_SIZE = 8;
 
-//representing the internal state
+// representing the internal state
 class Piece;
-using BoardState = array<array<Piece*, GRID_SIZE>, GRID_SIZE>;
+using BoardState = array<array<Piece *, GRID_SIZE>, GRID_SIZE>;
 
-//represnting the team of a piece
-typedef enum {WHITE, BLACK} Team;
-
-
+// represnting the team of a piece
+typedef enum { WHITE, BLACK } Team;
