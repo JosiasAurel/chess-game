2b65ec9bb5b9763d3edc8a92c1f0fad277147fc2
glenn-po <glennfor72@gmail.com>
2023-04-15 15:44:04 +0100

    Load sprites

--- main.cpp
+++ main.cpp
@@ -8,6 +8,28 @@
 // using std::cout;
 // using std::endl;
 
+//Load assets
+
+const map<PieceType, Image> blackPieces = {
+  {PieceType::PAWN, LoadImage("../assets/sprites/pawn_black.png")},
+  {PieceType::KNIGHT, LoadImage("../assets/sprites/knight_black.png")},
+  {PieceType::BISHOP, LoadImage("../assets/sprites/bishop_black.png")},
+  {PieceType::ROOK, LoadImage("../assets/sprites/rook_black.png")},
+  {PieceType::QUEEN, LoadImage("../assets/sprites/queen_black.png")},
+  {PieceType::KING, LoadImage("../assets/sprites/king_black.png")},
+};
+
+const map<PieceType, Image> whitePiece = {
+  {PieceType::PAWN, LoadImage("../assets/sprites/pawn_black.png")},
+  {PieceType::KNIGHT, LoadImage("../assets/sprites/knight_black.png")},
+  {PieceType::BISHOP, LoadImage("../assets/sprites/bishop_black.png")},
+  {PieceType::ROOK, LoadImage("../assets/sprites/rook_black.png")},
+  {PieceType::QUEEN, LoadImage("../assets/sprites/queen_black.png")},
+  {PieceType::KING, LoadImage("../assets/sprites/king_black.png")},
+};
+
+
+
 void DrawBoard(Board &board, int initW, int initH) {
   int xShift = initW;
   int yShift = initH;
