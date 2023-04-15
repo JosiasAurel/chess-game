61ee4ad1dfe495c22dab1cd1ef7d83ab3de4ffb5
glenn-po <glennfor72@gmail.com>
2023-04-15 17:20:36 +0100

    Complete placing of pieces on board

--- main.cpp
+++ main.cpp
@@ -11,21 +11,21 @@
 //Load assets
 
 const map<PieceType, Image> blackPieces = {
-  {PieceType::PAWN, LoadImage("../assets/sprites/pawn_black.png")},
-  {PieceType::KNIGHT, LoadImage("../assets/sprites/knight_black.png")},
-  {PieceType::BISHOP, LoadImage("../assets/sprites/bishop_black.png")},
-  {PieceType::ROOK, LoadImage("../assets/sprites/rook_black.png")},
-  {PieceType::QUEEN, LoadImage("../assets/sprites/queen_black.png")},
-  {PieceType::KING, LoadImage("../assets/sprites/king_black.png")},
+  {PieceType::PAWN, LoadImage("./assets/sprites/pawn_black.png")},
+  {PieceType::KNIGHT, LoadImage("./assets/sprites/knight_black.png")},
+  {PieceType::BISHOP, LoadImage("./assets/sprites/bishop_black.png")},
+  {PieceType::ROOK, LoadImage("./assets/sprites/rook_black.png")},
+  {PieceType::QUEEN, LoadImage("./assets/sprites/queen_black.png")},
+  {PieceType::KING, LoadImage("./assets/sprites/king_black.png")},
 };
 
-const map<PieceType, Image> whitePiece = {
-  {PieceType::PAWN, LoadImage("../assets/sprites/pawn_black.png")},
-  {PieceType::KNIGHT, LoadImage("../assets/sprites/knight_black.png")},
-  {PieceType::BISHOP, LoadImage("../assets/sprites/bishop_black.png")},
-  {PieceType::ROOK, LoadImage("../assets/sprites/rook_black.png")},
-  {PieceType::QUEEN, LoadImage("../assets/sprites/queen_black.png")},
-  {PieceType::KING, LoadImage("../assets/sprites/king_black.png")},
+const map<PieceType, Image> whitePieces = {
+  {PieceType::PAWN, LoadImage("./assets/sprites/pawn_white.png")},
+  {PieceType::KNIGHT, LoadImage("./assets/sprites/knight_white.png")},
+  {PieceType::BISHOP, LoadImage("./assets/sprites/bishop_white.png")},
+  {PieceType::ROOK, LoadImage("./assets/sprites/rook_white.png")},
+  {PieceType::QUEEN, LoadImage("./assets/sprites/queen_white.png")},
+  {PieceType::KING, LoadImage("./assets/sprites/king_white.png")},
 };
 
 
@@ -48,6 +48,28 @@ void DrawBoard(Board &board, int initW, int initH) {
   }
 }
 
+void DrawPieces(Board &board, int initW, int initH) {
+  int xShift = initW;
+  int yShift = initH;
+  // auto boardState = board.boardState;
+  for (int i = 0; i < GRID_SIZE; i++) {
+    for (int j = 0; j < GRID_SIZE; j++) {
+      auto piecePtr = board.boardState[i][j].get(); 
+      if (piecePtr == nullptr)
+        continue;
+      auto teamPieces = piecePtr->getTeam() == Team::CHESS_BLACK ?
+                        blackPieces : whitePieces;
+      auto pieceSprite = ImageCopy(teamPieces[piecePtr->getPieceType()]);
+      ImageResize(&pieceSprite, 50, 50);
+      DrawTexture(LoadTextureFromImage(pieceSprite), xShift + 5, yShift + 5, WHITE);
+      xShift += SQ_SIZE;
+    }
+    xShift = initW;
+    yShift += SQ_SIZE;
+  }
+}
+
+
 int main(void) {
   Board board = Board();
   auto boardRepr = board.representBoard();
@@ -69,6 +91,7 @@ int main(void) {
     ClearBackground(PURPLE);
 
     DrawBoard(board, initWidth, initHeight);
+    DrawPieces(board, initWidth, initHeight);
 
     EndDrawing();
   }
