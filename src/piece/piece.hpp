c1f287cbca75c15ac14f24b819974605f5935ce9
JosiasAurel <ndjosiasaurel@gmail.com>
2023-04-30 15:34:24 +0100

    doc each piece's movements

--- src/piece/piece.hpp
+++ src/piece/piece.hpp
@@ -30,12 +30,17 @@ public:
   virtual ~Piece(){};
 
   int getId() { return id; }
-  Team  getTeam(){return team; }
-  PieceType getPieceType(){return pieceType;}
+  Team getTeam() { return team; }
+  PieceType getPieceType() { return pieceType; }
   bool move(BoardState, pair<int, int>);
   virtual vector<pair<int, int>> getPossibleMoves(BoardState) = 0;
 };
 
+/*
+Moves one square ahead all the time
+May move two squares on the first move
+Can move one unit on left or right diagonal if there's an enemy piece
+*/
 class Pawn : public Piece {
 public:
   Pawn(pair<int, int> coord, Team team);
@@ -43,6 +48,11 @@ public:
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
 
+/*
+Moves such that it forms an L-shape
+Must land on the last square that contitutes the L
+Cannot move in a certain L-shape if there is a piece on one the path
+*/
 class Knight : public Piece {
 public:
   Knight(pair<int, int> coord, Team team);
@@ -50,6 +60,11 @@ public:
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
 
+/*
+Moves along the path of a cross
+Diagonal left extending forward and backward
+Diagonal right extending forward and backward
+*/
 class Bishop : public Piece {
 public:
   Bishop(pair<int, int> coord, Team team);
@@ -57,6 +72,9 @@ public:
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
 
+/*
+Moves so as to form a plus sign (both forward and backward)
+*/
 class Rook : public Piece {
 public:
   Rook(pair<int, int> coord, Team team);
@@ -64,6 +82,9 @@ public:
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
 
+/*
+Moves at most one square in all directions
+*/
 class King : public Piece {
 public:
   King(pair<int, int> coord, Team team);
@@ -71,6 +92,10 @@ public:
   vector<pair<int, int>> getPossibleMoves(BoardState);
 };
 
+/*
+Moves along a path that is a combination of a plus and an X sign
+i.e like a Rook and bishop combined
+*/
 class Queen : public Piece {
 public:
   Queen(pair<int, int> coord, Team team);
